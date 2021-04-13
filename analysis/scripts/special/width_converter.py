8b868836f449ccd0f7aa57c1a5f4cdea13f7747c
krellemeister <k.relle@live.dk>
2021-04-13 15:00:19 +0200

    latex update

--- analysis/scripts/special/width_converter.py
+++ analysis/scripts/special/width_converter.py
@@ -7,69 +7,110 @@ from matplotlib import pyplot as plt
 
 # input
 if not (len(sys.argv) == 2): 
-    print("Usage: python width_converter <reduced width amplitude in keV>")
+    print("Usage: python width_converter <reduced width amplitude in keV^(1/2)>")
 
-gamma = float(sys.argv[1])*1000 # reduced width amplitude to be converted
+gamma = float(sys.argv[1]) * mpmath.sqrt(1000*const.eV) # reduced width amplitude to be converted
 
 ### setup parameters ###
-Z1 = 2 # 4He
+# 4He
+Z1 = 2
 A1 = 2 
-Z2 = 4 # 8Be
-A2 = 4 
 m1 = 4.0026*const.u
+
+# 8Be
+Z2 = 4
+A2 = 4
 m2 = 8.0053*const.u
 
-beam = 2001*1000*const.eV # 2001keV
+E_res = 3030*1000*const.eV # resonance energy, 3030 keV
+E_beam = 2001*1000*const.eV # beam energy, 2001 keV
 l = 2 # l quantum number of the excited state of 8Be
 a_c = 1.4*const.femto*(mpmath.power(A1, 1.0/3) + mpmath.power(A2, 1.0/3)) # interaction radius
+M = m1*m2/(m1 + m2) # reduced mass
 
 ### calculate Gamma ###
 # definition of the sommerfeld parameter
-def sommerfeld(Z1, Z2, m1, m2, E):
-    reduced_mass = lambda m1, m2 : m1*m2/(m1 + m2)
-    return const.alpha*Z1*Z2*mpmath.sqrt(reduced_mass(m1, m2)*const.c**2/(2*E))
+def sommerfeld(rho):
+    return Z1*Z2*const.e**2*M*a_c/(4*const.pi*const.epsilon_0*const.hbar**2*rho)
 
 # momentum from beam energy
 def k(E): 
-    return mpmath.sqrt((m1**2 * const.c**2) - E**2/const.c**2) # E**2 = (mc**2)**2 + (pc)**2
-#    return mpmath.sqrt(2*m1*E)/const.hbar
+    return mpmath.sqrt(2*M*E)/const.hbar
 
 # the regular coulomb wave function
-def F(E):
-    eta = sommerfeld(Z1, Z2, m1, m2, E)
-    return mpmath.coulombf(l, eta, a_c)
+def F(l, eta, rho):
+    eta = sommerfeld(rho)
+    return const.hbar**2/(2*M)*mpmath.coulombf(l, eta, rho)
 
 # the irregular coulomb wave function
-def G(E):
-    eta = sommerfeld(Z1, Z2, m1, m2, E)
-    return mpmath.coulombg(l, eta, a_c)
+def G(l, eta, rho):
+    return const.hbar**2/(2*M)*mpmath.coulombg(l, eta, rho)
 
 # the penetrability factor
-def pen(E): 
-    rho = a_c*k(E)
-    f = F(E)
-    g = G(E)
+def pen(l, rho): 
+    eta = sommerfeld(rho)
+    f = F(l, eta, rho)
+    g = G(l, eta, rho)
     return rho/(f**2 + g**2)
 
 # the shift factor
-def shift(E):
-    f = F(E)
-    g = G(E)
-    fp = mpmath.diff(F, E) # df/dE
-    gp = mpmath.diff(G, E) # dg/dE
-    return pen(E)*(f*fp + g*gp)
+def shift(l, rho):
+    eta = sommerfeld(rho)
+    f = F(l, eta, rho)
+    g = G(l, eta, rho)
 
-S = shift(beam)
-Sp = mpmath.diff(shift, beam) # dS/dE
-P = pen(beam)
+    # mpmath.diff requires that the function depends only on a single variable
+    F_rho = lambda rho : F(l, eta, rho) 
+    G_rho = lambda rho : G(l, eta, rho) 
+    fp = mpmath.diff(F_rho, rho) # df/drho
+    gp = mpmath.diff(G_rho, rho) # dg/drho
 
-Gamma = 2 * gamma**2 * P/(1 + gamma**2 * Sp) / (1000*const.eV)
+    return pen(l, rho)*(f*fp + g*gp)
 
-# x = np.linspace(100, 200000, 100)
-# y = list(pen(z) for z in x)
-# plt.figure()
-# plt.plot(x, y)
-# plt.show()
+rho = a_c*k(E_res) # E_res or E_beam ?
+shift_rho = lambda rho : shift(l, rho)
+Sp = mpmath.diff(shift_rho, rho)*a_c*const.hbar*(-mpmath.power(E_res, -3.0/2)/(4*M)) # dS/dE
+P = pen(l, rho)
 
+print(gamma**2*P)
+print(gamma**2*Sp)
+
+Gamma = 2*gamma**2*P/(1 + gamma**2*Sp) / (1000*const.eV) # convert to keV
 print(f"The ordinary width corresponding to the reduced width {sys.argv[1]} keV^½ is")
 print(f"{Gamma} keV")
+
+x = np.linspace(0.01, 2, 50)
+y1 = list(pen(0, z) for z in x)
+y2 = list(pen(1, z) for z in x)
+y3 = list(pen(2, z) for z in x)
+y4 = list(pen(3, z) for z in x)
+y5 = list(pen(4, z) for z in x)
+
+z1 = list(shift(0, z) for z in x)
+z2 = list(shift(1, z) for z in x)
+z3 = list(shift(2, z) for z in x)
+z4 = list(shift(3, z) for z in x)
+z5 = list(shift(4, z) for z in x)
+
+plt.figure()
+plt.title("Penetration factor")
+plt.xlabel("rho")
+plt.ylabel("Pl")
+plt.plot(x, y1, label="l = 0")
+plt.plot(x, y2, label="l = 1")
+plt.plot(x, y3, label="l = 2")
+plt.plot(x, y4, label="l = 3")
+plt.plot(x, y5, label="l = 4")
+plt.legend()
+
+plt.figure()
+plt.title("Shift factors")
+plt.xlabel("rho")
+plt.ylabel("Sl")
+plt.plot(x, z1, label="l = 0")
+plt.plot(x, z2, label="l = 1")
+plt.plot(x, z3, label="l = 2")
+plt.plot(x, z4, label="l = 3")
+plt.plot(x, z5, label="l = 4")
+plt.legend()
+plt.show()
